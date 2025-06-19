7f5b4d6f994cc6e94b3ca7c74c20028cd7f1f219
william dew <williamrdew@gmail.com>
2025-06-19 15:04:36 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Maui.Controls;
+using System.ComponentModel;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private string selectedItemDetail;
+        public string SelectedItemDetail
+        {
+            get => selectedItemDetail;
+            set
+            {
+                if (selectedItemDetail != value)
+                {
+                    selectedItemDetail = value;
+                    OnPropertyChanged(nameof(SelectedItemDetail));
+
+                }
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        // Event handler for the "Add" button
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            // Add your logic here
+        }
+
+        // Event handler for the "Remove" button
+        private void OnRemoveButtonClicked(object sender, EventArgs e)
+        {
+            // Add your logic here
+        }
+
+        // Event handler for the "Edit" button
+        private void OnEditButtonClicked(object sender, EventArgs e)
+        {
+            // Add your logic here
+        }
+    }
+}
---
+++ MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiApp2
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _selectedItemDetail = "";
+
+        public string SelectedItemDetail
+        {
+            get { return _selectedItemDetail; }
+            set
+            {
+                if (_selectedItemDetail == value) return;
+                _selectedItemDetail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string
+propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new
+PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
